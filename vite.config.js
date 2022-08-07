50615ac5d5875d7249fdb555cc4a0f2f471e9cce
Durdsoft <david.p.shep@gmail.com>
2022-08-07 14:29:04 +0100

    output to correct directory

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     root: "src/Docs",
     build: {
-        outDir: "../../.fable-build",
+        outDir: "./publish/docs",
         emptyOutDir: true,
         sourcemap: true
     },
