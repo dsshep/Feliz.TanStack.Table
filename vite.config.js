5d9baa240c3ffa1b360336eba0f94904fc652e9e
Durdsoft <david.p.shep@gmail.com>
2022-08-07 14:37:26 +0100

    correct directory

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     root: "src/Docs",
     build: {
-        outDir: "./publish/docs",
+        outDir: "../../publish/docs",
         emptyOutDir: true,
         sourcemap: true
     },
